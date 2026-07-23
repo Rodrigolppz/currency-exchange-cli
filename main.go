b27f61aa375b220bff4b38ff0a3d075e9c1bb417
Rodrigolppz <rodrigobracens@gmail.com>
2026-07-23 11:42:42 -0300

    add some comment

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ func DecodeJson(resp []byte) Currency {
 
 	var c Currency
 
-	err := json.Unmarshal(resp, &c)
+	err := json.Unmarshal(resp, &c) // Unmarshal function from encoding/json library, used to Decode raw Json and return the value to c
 	if err != nil {
 		fmt.Println("Error")
 	}
@@ -48,6 +48,7 @@ func FindCurrency(currency Currency, searchKey string) float64 {
 	var save float64
 
 	_, exists := currency.ConversionRates[searchKey] // this is a "comma ok idiom" or "map lookup", its a native golang expression to search for values in a map.
+	// return the value associated with the key and a bool if the key exist or not.
 
 	if exists {
 		save = currency.ConversionRates[searchKey]
